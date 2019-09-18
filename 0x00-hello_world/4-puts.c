353654938e4bf70536a5bf28d50dc251f4df19ed
Raudo <rivaspaulino@outlook.com>
2019-09-18 22:05:04 +0000

    first C program!

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.>
+/**
+ * main-entry to program
+ * return:0
+ * Return: of value main: void
+ *
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

bc530a14f5ecf2dfb15766939b3ad83b2b0543e4
Raudo <rivaspaulino@outlook.com>
2019-09-19 20:24:30 +0000

    This program will determine if the variable n is positive, negative, or zero, each time itis executed.

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,15 +15,15 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("is positive\n");
+		printf("%i is positive\n");
 	}
 	else if (n == 0)
 	{
-		printf("is zero\n");
+		printf("%i is zero\n");
 	}
 	else if (n < 0)
 	{
-		printf("is negative\n");
+		printf("%i is negative\n");
 	}
 	return (0);
 }

634ba88694ed07c9e57b6664a39b1b760b65d163
Raudo <rivaspaulino@outlook.com>
2019-09-19 20:13:56 +0000

    This program will determine if the variable n is positive, negative, or zero, each time itis executed.

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - print positive, negative, and zero depending o the number
+ * Description: this program prints "positive, negative, and zero"
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("is positive\n");
+	}
+	else if (n == 0)
+	{
+		printf("is zero\n");
+	}
+	else if (n < 0)
+	{
+		printf("is negative\n");
+	}
+	return (0);
+}

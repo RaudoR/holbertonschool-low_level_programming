4d3a3c7ea4f00312283b885c53145d197cd41115
Raudo <rivaspaulino@outlook.com>
2019-09-26 18:11:36 +0000

    functions

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,8 +1,10 @@
 #ifndef HOLBERTON_H_INCLUDED
 #define HOLBERTON_H_INCLUDED
 
+int _putchar(char);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
 
 #endif

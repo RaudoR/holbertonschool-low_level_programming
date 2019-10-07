3094a15295443321c496adb069b6379faac9861b
Raudo <rivaspaulino@outlook.com>
2019-10-07 22:02:18 +0000

    Write a function that locates a character in a string.

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _strchr - main function
+ * @s: array
+ * @c: first occurrence
+ * Return: &s
+ */
+char *_strchr(char *s, char c)
+
+{
+
+	int zulu;
+
+	for (zulu = 0; s[zulu] != '\0'; zulu++)
+	{
+		if (s[zulu] == c)
+			return (&s[zulu]);
+	}
+		return ('\0');
+}

b584b58b268547208c9ddc6fcdb25cdc0f02ff70
RaudoR <rivaspaulino@outlook.com>
2019-10-25 02:58:45 +0000

    debugged file

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,7 +21,7 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-	while (ops[1].op != NULL && strcmp(ops[i].op, s) != 0)
+	while (ops[i].op != NULL && strcmp(ops[i].op, s) != 0)
 	{
 		i++;
 	}

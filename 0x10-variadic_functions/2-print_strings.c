60f46b9c619b2d2f92dc8b52b821f25b9366a300
RaudoR <rivaspaulino@outlook.com>
2019-10-28 01:44:14 +0000

    added base case

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -30,7 +30,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		}
 		if (separator != NULL && (alpha < (n - 1)))
-                        printf("%s", separator);
+			printf("%s", separator);
 	}
 	va_end(lst);
 	printf("\n");

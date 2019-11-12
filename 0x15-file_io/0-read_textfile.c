6ec24b718ef6274966df30249cbd9a04e11c5ce9
RaudoR <rivaspaulino@outlook.com>
2019-11-12 22:36:55 +0000

    a function that reads a text file and prints it to the POSIX standard output.

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * read_textfile - this function will read a text file
+ * @filename: the file to be read
+ * @letters: to be printed
+ * Return: rd
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+	int fd;
+	ssize_t rd, wrt;
+	char *pletters = malloc(sizeof(char) * letters);
+
+	if (filename == NULL)
+		return (0);
+	if (pletters == NULL)
+		return (-1);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (-1);
+
+	rd = read(fd, pletters, letters);
+	if (rd == -1)
+		return (-1);
+
+	wrt = write(STDOUT_FILENO, pletters, rd);
+	if (wrt == -1)
+		return (-1);
+	close(fd);
+	return (rd);
+}
